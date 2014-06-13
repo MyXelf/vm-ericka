b854087c0c6b67cc1540e428dc08517615dd86e9
Jota Jota <jasmyxelf@gmail.com>
2014-06-13 08:37:06 -0400

    Implement third-level submenu in the Sidebar

--- js/vm-rdesign.js
+++ js/vm-rdesign.js
@@ -16,3 +16,13 @@ $('.vm-sidebar-toggle').click(function () {
   $('body').toggleClass('vm-sidebar-expanded')
 })
 
+
+/**
+ * Third Level Submenu
+ */
+$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  $(this).parent().siblings().removeClass('open');
+  $(this).parent().toggleClass('open');
+})
